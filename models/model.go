9a3f1520c1dcc2f21f73713470f334dd22c5ad60
mnnaegel <maxnaegel@gmail.com>
2023-01-10 18:50:59 -0500

    Init

---
+++ models/model.go
@@ -0,0 +1,8 @@
+package model
+
+// create a model for a review with the fields: professor, quality, difficulty, date, reviewText, course
+type Review struct {
+	
+}
+
+// create a model for a professor with the fields: name, rating, numRatings, department, level of difficulty, and reviews
---
+++ worker/scrape.go
@@ -0,0 +1,4 @@
+package worker
+
+// Create a worker that will scrape the data from this website `https://www.ratemyprofessors.com/school?sid=1491`
+// and store the data in an array
