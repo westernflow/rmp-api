e05a1a97ac131edd2799819a588fa037ea3c2c29
mnnaegel <maxnaegel@gmail.com>
2023-01-14 20:22:51 -0500

    Added PostgreSQL & dockerized application

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
+	// "fmt"
+	"database/sql"
+	"fmt"
 	"rmpParser/worker"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	// call the scraper
-	worker.PopulateDB()
+	db, err := sql.Open("postgres", "postgres://postgres@db:5432/postgres?sslmode=disable")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+	// ping the database to check if it is alive
+	fmt.Println("Pinging the database...")
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+	// create the database
+	_, err = db.Exec("CREATE DATABASE mydb")
+    if err != nil {
+        panic(err)
+    }
+
+	// get all departments from the school
+	departments := worker.GetDepartments()
+	// fmt.Println(departments)
+	// get all professors from each department
+	for _, department := range departments {
+		worker.AddProfessorsFromDepartmentToDatabase(department.ID)
+	}
 }
