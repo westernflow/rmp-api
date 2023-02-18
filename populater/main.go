8979d537e1da5b8a0232480f2575ecd3f3027564
mnnaegel <maxnaegel@gmail.com>
2023-02-18 07:48:42 -0500

    Added feature to scrape particular department/departments

--- populater/main.go
+++ populater/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"rmpParser/controller"
 	"rmpParser/handler"
+	model "rmpParser/models"
 	"rmpParser/worker"
 
 	"github.com/joho/godotenv"
@@ -33,6 +34,27 @@ func getData() {
 	// get all departments
 	departments := worker.GetDepartments()
 	
+	// historyAndOnwards := getDepartmentSplice(departments, "history")
+
 	// populate the database (dont do this if you already have data in the database)
 	c.PopulateDatabase(departments)
+}
+
+func getDepartment(departments []model.Department, name string) []model.Department {
+	for i, department := range departments {
+		if department.Name == name {
+			return departments[i:i+1]
+		}
+	}
+	return []model.Department{}
+}
+
+func getDepartmentSplice(departments []model.Department, name string) []model.Department {
+	// finds first occurence of name, returns all departments after that
+	for i, department := range departments {
+		if department.Name == name {
+			return departments[i:]
+		}
+	}
+	return []model.Department{}
 }
