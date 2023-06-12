2d31dab9add7493978084aacb269d9582ad6093d
Felo Tadros <felotadros326@gmail.com>
2023-06-12 15:36:34 -0400

    update to match wflow

--- uwomodel/course.go
+++ uwomodel/course.go
@@ -2,7 +2,7 @@ package uwomodel
 
 // TimeComponent represents a single meeting time for a course section
 type TimeComponent struct {
-	Day       string `bson:"days" json:"days" example:"M"`
+	Days       string `bson:"days" json:"days" example:"M"`
 	StartTime string `bson:"startTime" json:"startTime" example:"8:30 AM"`
 	EndTime   string `bson:"endTime" json:"endTime" example:"1:30 PM"`
 }
@@ -13,7 +13,7 @@ type SectionComponent struct {
 	Component   string          `bson:"component" json:"component" example:"LEC"`
 	ClassNumber int             `bson:"classNumber" json:"classNumber" example:"5000"`
 	Location    string          `bson:"location" json:"location" example:"NS 145"`
-	Instructor  string          `bson:"instructor" json:"instructor" example:"Haffie"`
+	Instructors  []string          `bson:"instructors" json:"instructors" example:"Haffie"`
 	Reqs        string          `bson:"requisites" json:"requisites" example:"REQUISITES:..."`
 	Status      string          `bson:"status" json:"status" example:"Full"`
 	Campus      string          `bson:"campus" json:"campus" example:"Main"`
@@ -24,7 +24,7 @@ type SectionComponent struct {
 // CourseComponent - represents the specific data common to all courses sections of any given course
 type CourseComponent struct {
 	Faculty     string `bson:"faculty" json:"faculty" example:"CLASSICS"`
-	Number      int    `bson:"number" json:"number" example:"2053"`
+	Number      string `bson:"number" json:"number" example:"2053"`
 	Suffix      string `bson:"suffix" json:"suffix" example:"B"`
 	Name        string `bson:"name" json:"name" example:"MATH FOR FINANCIAL ANALYSIS"`
 	Description string `bson:"description" json:"description" example:"Course description"`
@@ -32,7 +32,6 @@ type CourseComponent struct {
 
 // Course - Returned as endpoint only, stores the information of a course and all its related section information
 type Course struct {
-	ID          string             `bson:"_id" json:"id"`
 	Source      SourceInfo         `bson:"source" json:"source"`
 	Time        TimeInfo           `bson:"time" json:"time"`
 	CourseData  CourseComponent    `bson:"courseData" json:"courseData"`
--- uwomodel/professor.go
+++ uwomodel/professor.go
@@ -1,23 +1,22 @@
 package uwomodel
 
+// Struct for a Western professor
 type Professor struct {
-	Name           string   `json:"name" bson:"name"`
-	RMPName        string   `json:"rmpName" bson:"rmpName"`
-	RMPId          string   `json:"rmpId" bson:"rmpId"`
-	Rating         float64  `json:"rating" bson:"rating"`
-	Difficulty     float64  `json:"difficulty" bson:"difficulty"`
-	CurrentCourses []string `json:"currentCourses" bson:"currentCourses"`
-	Reviews        []Review `json:"reviews" bson:"reviews"`
-	Departments    []string `json:"departments" bson:"departments"`
-}
-
-type Review struct {
-	ProfessorID string  `json:"professorId" bson:"professorId"`
-	Professor   string  `json:"professor" bson:"professor"`
-	Quality     float64 `json:"quality" bson:"quality"`
-	Difficulty  float64 `json:"difficulty" bson:"difficulty"`
-	Date        string  `json:"date" bson:"date"`
-	ReviewText  string  `json:"reviewText" bson:"reviewText"`
-	Helpful     float64 `json:"helpful" bson:"helpful"` // quality = helpful+clarity/2
-	Clarity     float64 `json:"clarity" bson:"clarity"`
+	ID             string   `bson:"_id" json:"id"`
+	Name           string   `bson:"name" json:"name" examples:"J. Smith"`
+	RMPName        string   `bson:"rmpName" json:"rmpName" examples:"John Smith"`
+	RMPId          string   `bson:"rmpId" json:"rmpId" examples:"VGVhY2hlci0yMTM5MjE0"`
+	Rating         float64  `bson:"rating" json:"rating" examples:"4.5"`
+	Difficulty     float64  `bson:"difficulty" json:"difficulty" examples:"3.5"`
+	CurrentCourses []string `bson:"currentCourses" json:"currentCourses" examples:"[\"COSC 101\", \"MATH 102\"]"`
+	Reviews        []struct {
+		ProfessorID string `bson:"professorId" json:"professorId" examples:"VGVhY2hlci0yMTM5MjE0"`
+		Quality     int    `bson:"quality" json:"quality" examples:"4"`
+		Difficulty  int    `bson:"difficulty" json:"difficulty" examples:"5"`
+		Date        string `bson:"date" json:"date" examples:"2018-02-07 10:28:42 +0000 UTC"`
+		ReviewText  string `bson:"reviewText" json:"reviewText" examples:"This course was very interesting and the professor was very helpful."`
+		Helpful     int    `bson:"helpful" json:"helpful" examples:"3"`
+		Clarity     int    `bson:"clarity" json:"clarity" examples:"5"`
+	} `bson:"reviews" json:"reviews"`
+	Departments []string `bson:"departments" json:"departments" examples:"[\"COSC\", \"MATH\"]"`
 }
