9dca29677fc252b2e35a78f8ab7d0c1163a6971a
Felo Tadros <felotadros326@gmail.com>
2023-06-17 12:48:01 -0400

    Add Cursor to model

--- models/model.go
+++ models/model.go
@@ -63,36 +63,37 @@ type DepartmentPageData struct {
 }
 
 type TeacherSearchResults struct {
-    Data struct {
-        School struct {
-            ID   string `json:"id"`
-            Name string `json:"name"`
-        } `json:"school"`
-        Search struct {
-            Teachers struct {
-                Edges []struct {
-                    Node struct {
-                        FirstName string `json:"firstName"`
-                        ID        string `json:"id"`
-                        LastName  string `json:"lastName"`
-                        Ratings   struct {
-                            Edges []struct {
-                                Node struct {
-                                    ClarityRating    int     `json:"clarityRating"`
-                                    Comment          string  `json:"comment"`
-                                    Date             string  `json:"date"`
-                                    DifficultyRating int     `json:"difficultyRating"`
-                                    HelpfulRating    int     `json:"helpfulRating"`
-                                    ID               string  `json:"id"`
-                                    QualityRating    int     `json:"qualityRating"`
-                                } `json:"node"`
-                            } `json:"edges"`
-                        } `json:"ratings"`
-                    } `json:"node"`
-                } `json:"edges"`
-            } `json:"teachers"`
-        } `json:"search"`
-    } `json:"data"`
+	Data struct {
+		School struct {
+			ID   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"school"`
+		Search struct {
+			Teachers struct {
+				Edges []struct {
+					Cursor string `json:"cursor"`
+					Node   struct {
+						FirstName string `json:"firstName"`
+						ID        string `json:"id"`
+						LastName  string `json:"lastName"`
+						Ratings   struct {
+							Edges []struct {
+								Node struct {
+									ClarityRating    int    `json:"clarityRating"`
+									Comment          string `json:"comment"`
+									Date             string `json:"date"`
+									DifficultyRating int    `json:"difficultyRating"`
+									HelpfulRating    int    `json:"helpfulRating"`
+									ID               string `json:"id"`
+									QualityRating    int    `json:"qualityRating"`
+								} `json:"node"`
+							} `json:"edges"`
+						} `json:"ratings"`
+					} `json:"node"`
+				} `json:"edges"`
+			} `json:"teachers"`
+		} `json:"search"`
+	} `json:"data"`
 }
 
 type ProfessorData struct {
