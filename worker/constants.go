2059116b2b3896366ce2ddb0edd886a8c9ad317a
Felo Tadros <felotadros326@gmail.com>
2023-06-16 20:59:07 -0400

    Adding profquery

--- worker/constants.go
+++ worker/constants.go
@@ -47,7 +47,7 @@ const HomePageQuery = `query TeacherSearchResultsPageQuery(
 	  id
 	}
   }
-  
+
   fragment TeacherSearchPagination_search_1ZLmLD on newSearch {
 	teachers(query: $query, first: 8, after: "") {
 	  didFallback
@@ -73,7 +73,7 @@ const HomePageQuery = `query TeacherSearchResultsPageQuery(
 	  }
 	}
   }
-  
+
   fragment TeacherCard_teacher on Teacher {
 	id
 	legacyId
@@ -84,12 +84,12 @@ const HomePageQuery = `query TeacherSearchResultsPageQuery(
 	...CardName_teacher
 	...TeacherBookmark_teacher
   }
-  
+
   fragment CardFeedback_teacher on Teacher {
 	wouldTakeAgainPercent
 	avgDifficulty
   }
-  
+
   fragment CardSchool_teacher on Teacher {
 	department
 	school {
@@ -97,18 +97,18 @@ const HomePageQuery = `query TeacherSearchResultsPageQuery(
 	  id
 	}
   }
-  
+
   fragment CardName_teacher on Teacher {
 	firstName
 	lastName
   }
-  
+
   fragment TeacherBookmark_teacher on Teacher {
 	id
 	isSaved
   }`
 const DepartmentQuery = "query TeacherSearchResultsPageQuery(\n  $query: TeacherSearchQuery!\n  $schoolID: ID\n) {\n  search: newSearch {\n    ...TeacherSearchPagination_search_1ZLmLD\n  }\n  school: node(id: $schoolID) {\n    __typename\n    ... on School {\n      name\n    }\n    id\n  }\n}\n\nfragment TeacherSearchPagination_search_1ZLmLD on newSearch {\n  teachers(query: $query, first: 3000, after: \"\") {\n    didFallback\n    edges {\n      cursor\n      node {\n        ...TeacherCard_teacher\n        id\n        __typename\n      }\n    }\n    pageInfo {\n      hasNextPage\n      endCursor\n    }\n    resultCount\n    filters {\n      field\n      options {\n        value\n        id\n      }\n    }\n  }\n}\n\nfragment TeacherCard_teacher on Teacher {\n  id\n  legacyId\n  avgRating\n  numRatings\n  ...CardFeedback_teacher\n  ...CardSchool_teacher\n  ...CardName_teacher\n  ...TeacherBookmark_teacher\n}\n\nfragment CardFeedback_teacher on Teacher {\n  wouldTakeAgainPercent\n  avgDifficulty\n}\n\nfragment CardSchool_teacher on Teacher {\n  department\n  school {\n    name\n    id\n  }\n}\n\nfragment CardName_teacher on Teacher {\n  firstName\n  lastName\n}\n\nfragment TeacherBookmark_teacher on Teacher {\n  id\n  isSaved\n}\n"
-
+const AllProfsAndReviewsQuery = `{"query":"query TeacherSearchResultsPageQuery(\r\n\t$query: TeacherSearchQuery!\r\n\t$schoolID: ID\r\n  ) {\r\n\tsearch: newSearch {\r\n\t  ...TeacherSearchPagination_search_1ZLmLD\r\n\t}\r\n\tschool: node(id: $schoolID) {\r\n\t#   __typename\r\n\t  ... on School {\r\n\t\tname\r\n\t  }\r\n\t  id\r\n\t}\r\n  }\r\n  \r\n  fragment TeacherSearchPagination_search_1ZLmLD on newSearch {\r\n\tteachers(query: $query, first: 1, after: \"\") {\r\n\t  edges {\r\n\t\tnode {\r\n\t\t  ...TeacherCard_teacher\r\n\t\t  id\r\n        ratings {\r\n            edges{\r\n                node{\r\n                    qualityRating\r\n                    difficultyRating\r\n                    date\r\n                    comment\r\n                    helpfulRating\r\n                    clarityRating\r\n                    id\r\n                }\r\n            }\r\n        }\r\n\t\t}\r\n\t  }\r\n\t}\r\n  }\r\n  \r\n  fragment TeacherCard_teacher on Teacher {\r\n\tid\r\n\t...CardName_teacher\r\n  }\r\n  \r\n  \r\n  fragment CardName_teacher on Teacher {\r\n\tfirstName\r\n\tlastName\r\n  }","variables":{"query":{"text":"","schoolID":"U2Nob29sLTE0OTE=","fallback":false},"schoolID":"U2Nob29sLTE0OTE="}}`
 const WesternID = "U2Nob29sLTE0OTE="
 
 const AuthID = "dGVzdDp0ZXN0"
