45bf7185da1e5857eae50baf6e28eb1874b13c92
Felo Tadros <felotadros326@gmail.com>
2023-06-18 16:18:46 -0400

    Lambda cleanup

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/joho/godotenv"
 
 	mongocontroller "rmpParser/mongoController"
-	"rmpParser/worker"
 )
 
 func main() {
@@ -19,5 +18,4 @@ func main() {
 	// populate the database (dont do this if you already have data in the database)
 	// c.InitializeDatabase()
 	// c.PopulateDatabase()
-	worker.Scrape()
 }
